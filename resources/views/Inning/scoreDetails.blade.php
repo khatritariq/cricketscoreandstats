e15ad00d4bf5a16c1fb1dd9ccf1a896d8a57c62d
Tariq Mehmood <kha3tariq@gmail.com>
2019-10-27 20:32:19 +0500

    View rendering

--- resources/views/Inning/scoreDetails.blade.php
+++ resources/views/Inning/scoreDetails.blade.php
@@ -2,19 +2,121 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             @if($inningData)
-
+                <h2>
                 <div >
                     Match between
-                    <b>{{$inningData['match']['team1']['name'] }}</b>
+                    <b style="color:red;">{{$inningData['match']['team1']['name'] }}</b>
                     and
-                    <b>{{$inningData['match']['team2']['name'] }}</b>
+                    <b style="color:red;">{{$inningData['match']['team2']['name'] }}</b>
+                    at
+                    <b style="color:blue;">{{ $inningData['match']['venue'] }}</b>
                 </div>
+                </h2>
+        <div>
+            <h3>Inning details</h3>
+            <b>Batting Stats</b>
+
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <td>Player</td>
+                            <td>Runs</td>
+                            <td>Balls</td>
+                            <td>Strike Rate</td>
+                            <td>Fours</td>
+                            <td>Sixes</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($inningData['playerbattingstat'] as $playerstats)
+                        <tr>
+                            <td>
+                                {{ $playerstats['participant']['teamplayer']['player']['name'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['runs'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['balls'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['strike_rate'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['fours'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['sixes'] }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+        </div>
+<br>
+                <b>Bowling Stats</b>
+
+                <table border="1">
+                    <thead>
+                    <tr>
+                        <td>Player</td>
+                        <td>Overs</td>
+                        <td>Maidens</td>
+                        <td>Runs</td>
+                        <td>Wickets</td>
+                        <td>Economy</td>
+                        <td>0s</td>
+                        <td>4s</td>
+                        <td>6s</td>
+                        <td>Wides</td>
+                        <td>No balls</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($inningData['playerbowlingstat'] as $playerstats)
+                        <tr>
+                            <td>
+                                {{ $playerstats['participant']['teamplayer']['player']['name'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['overs'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['maidens'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['runs'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['wickets'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['economy'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['zeros'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['fours'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['sixes'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['wide_ball'] }}
+                            </td>
+                            <td>
+                                {{ $playerstats['no_ball'] }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+        </div>
 
 
-            @foreach($inningData['playerbattingstat'] as $playerbatting)
-
-
-            @endforeach
             @endif
 
 
